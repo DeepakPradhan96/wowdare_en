a2e72bbe45186a2e8a00b38fc977c040f58014c0
DeepakPradhan96 <deeep040@gmail.com>
2021-10-19 19:47:41 +0530

    Wowdare

--- Feature/environment.py
+++ Feature/environment.py
@@ -6,7 +6,8 @@ def before_all(context):
     path='C:\\Driver\\chromedriver_win32\\chromedriver.exe'
     context.driver =Chrome(executable_path=path)
 
-    context.driver2=webdriver.Firefox()
+    Path="C:\Users\deepaak\PycharmProjects\Wowdare_en\Feature\Driver\geckodriver.exe"
+    context.driver2=webdriver.Firefox(executable_path=Path)
 
 
 def after_all(context):
